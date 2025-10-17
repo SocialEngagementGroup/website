56bcfcfba890e3387925b8e393d833734b05368e
sunit sen <sunitsen50@gmail.com>
2025-10-17 19:28:04 +0600

    icon check

--- components/FooterTop/ContactInfo/ContactInfo.js
+++ components/FooterTop/ContactInfo/ContactInfo.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { ArrowUpRight } from "lucide-react";
+import { ArrowUpRight } from "lucide-react"; // Using lucide icon for arrow
 import BoxTitle from "./BoxTitle";
 import style from "./ContactInfo.module.css";
 
-// ✅ Contact info data (icons, titles, alt text)
-const contactData = [
-  {
-    id: 1,
-    icon: "/icons/callicon.png",
-    title: "Book A Call",
-    alt: "Call Icon",
-  },
-  {
-    id: 2,
-    icon: "/icons/mailicon.png",
-    title: "Send Email",
-    alt: "Mail Icon",
-  },
-];
-
+const icons = {
+  call: "/icons/callicon.png",
+  email: "/icons/emailicon.png",
+}
 const ContactInfo = () => {
   return (
     <div className={`${style.contentbox} container-fluid mx-auto`}>
@@ -28,27 +16,46 @@ const ContactInfo = () => {
         {/* Section Title */}
         <BoxTitle />
 
-        {/* Flex container for contact boxes */}
+        {/* Flex container for Call & Email Boxes */}
         <div className="flex flex-col md:flex-row gap-6 mt-8">
-          {contactData.map((item) => (
-            <div
-              key={item.id}
-              className={`${style.contentboxInnerBox} border-4 border-gray-700 p-10 rounded-[20px] flex 
-              items-center justify-between cursor-pointer hover:shadow-lg transition-shadow flex-1 relative`}
-            >
-              <div className="flex flex-col items-start gap-4">
-                <img src={item.icon} alt={item.alt} width={60} height={60} />
-                <h4 className="font-semibold text-[32px] text-white">
-                  {item.title}
-                </h4>
-              </div>
 
-              {/* Arrow icon */}
-              <div className="absolute top-4 right-4">
-                <ArrowUpRight className="text-gray-700 dark:text-gray-300" size={40} />
-              </div>
+          {/* Call Box */}
+          <div
+            className={`${style.contentboxInnerBox} border-4 border-gray-700 p-10 rounded-[20px] flex 
+            items-center justify-between cursor-pointer hover:shadow-lg transition-shadow flex-1 relative`}
+          >
+            <div className="flex flex-col items-start gap-4">
+              <img src="/icons/callicon.png" alt="Call Icon" width={60} height={60} />
+              <h4 className="font-semibold text-[32px]  text-white">
+                Book A Call
+              </h4>
             </div>
-          ))}
+
+            {/* Arrow icon */}
+            <div className="absolute top-4 right-4">
+              <ArrowUpRight className="text-gray-700 dark:text-gray-300" size={40} />
+            </div>
+          </div>
+
+          {/* Mail Box */}
+          <div
+            className={`${style.contentboxInnerBox} border-4 border-gray-700 p-10 rounded-[20px] flex items-center justify-between cursor-pointer hover:shadow-lg transition-shadow flex-1 relative`}
+          >
+            <div className="flex flex-col items-start gap-4">
+              <img src="/icons/mailicon.png" alt="Mail Icon" width={60} height={60} />
+              <h4 className="font-semibold text-[32px]  dark:text-white text-white">
+                Send Email
+              </h4>
+            </div>
+
+            {/* Arrow icon */}
+            <div className="absolute top-4 right-4">
+              <ArrowUpRight className="text-gray-700 dark:text-gray-300" size={40} />
+              <img src="/icons/arrow-up-right-1.png" alt="Mail Icon" width={60} height={60} />
+
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
