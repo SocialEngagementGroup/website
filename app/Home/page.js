87ae7c81531a9c625dc6419435ef4c0da480e9b1
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2025-11-17 23:56:35 +0600

    add google-site-verification

--- app/Home/page.js
+++ app/Home/page.js
@@ -1,5 +1,3 @@
 // app/page.js is always treated as the root (home) page.
-
 // This means when someone visits your site at the / URL, Next.js will automatically look for app/page.js.
-
 // If you create a separate file named Home (app/home/page.js), it will be treated as the /home route, not the / route.
