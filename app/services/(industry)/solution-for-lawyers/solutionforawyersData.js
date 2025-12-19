3d48f53e4a0db6fb9eb85e9496009767e3ebf622
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2025-12-20 00:28:13 +0600

    update vdo link

--- app/services/(industry)/solution-for-lawyers/solutionforawyersData.js
+++ app/services/(industry)/solution-for-lawyers/solutionforawyersData.js
@@ -64,7 +64,7 @@ export const solutionforawyersData = {
         text: "You dominate local and practice-area visibility, outshining competitors.",
       },
     ],
-    video: "/videos/services/industry/solution-for-lawyers.mp4",
+    video: "/videos/services/industry/Solution-for-Lawyers.mp4",
   },
 
   BrandStrategy: {
