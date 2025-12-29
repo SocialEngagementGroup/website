41428b836f08b60b0226c78778cee993f7a2ed91
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2025-12-29 21:08:55 +0600

    update slider service page vdo replace hero button text use bg and faq section

--- app/services/(masterPage)/page.js
+++ app/services/(masterPage)/page.js
@@ -1,30 +1,31 @@
-import React from 'react';
-import HeroSection from '@/components/MasterServicesPage/HeroSection/HeroSection';
-import AutoSlider from '@/components/MasterServicesPage/AutoSlider/AutoSlider';
+import React from "react";
+import HeroSection from "@/components/MasterServicesPage/HeroSection/HeroSection";
+import AutoSlider from "@/components/MasterServicesPage/AutoSlider/AutoSlider";
 import { CreativeData, Digital, Industry } from "@/app/services/(masterPage)/sliderData";
-import styles from './master.module.css';
-
-// Function to ensure no repetition if slides count is already fine
+import styles from "./master.module.css";
 
 const MasterServicesPage = () => {
-  const sliders = [
-    { title: "Creative", data: CreativeData },
-    { title: "Digital", data: Digital },
-    { title: "Industry", data:Industry }, // No repetition for Industry slider
-  ];
-
   return (
     <>
       <HeroSection />
       <div className={styles.fade}></div>
-      <div className='bg-[#232326'>
-        <div className="max-w-[90%] w-full mx-auto py-5 md:py-10 px-3 md:px-10">
-          {sliders.map((slider, index) => (
-            <section key={index}>
-              <h3 className="text-white mt-5">{slider.title}</h3>
-              <AutoSlider slides={slider.data} />
-            </section>
-          ))}
+
+      <div className="bg-[#232326] py-8">
+        <div className="w-[90%] mx-auto">
+          <section className="py-4 mt:4  md:mt-10">
+            <h3 className="text-white mb-8">Creative</h3>
+            <AutoSlider slides={CreativeData} />
+          </section>
+
+          <section className="py-4 mt:4  md:mt-10">
+            <h3 className="text-white mb-8">Digital</h3>
+            <AutoSlider slides={Digital} />
+          </section>
+
+          <section className="py-4 mt:4  md:mt-10">
+            <h3 className="text-white mb-8">Industry</h3>
+            <AutoSlider slides={Industry} />
+          </section>
         </div>
       </div>
     </>
