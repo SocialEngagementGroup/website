6b98ee532344da80d8fadadc836e37d4b6b06f1c
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2025-12-30 02:28:43 +0600

    Uploaded the images

--- app/services/(masterPage)/sliderData.js
+++ app/services/(masterPage)/sliderData.js
@@ -1,10 +1,10 @@
 export const CreativeData = [
-  { title: "3D animation & rendering", bg: "services/masterimagesslider/creative/3D-Animation-&-Rendering.jpg", link: "services/3D-animation-and-rendering" },
-  { title: "Branding", bg: "services/masterimagesslider/creative/Branding.jpg", link: "/services/branding" },
-  { title: "Content Creation", bg: "services/masterimagesslider/creative/Content-Creation.jpg", link: "/services/content-creation" },
-  { title: "Logo Design", bg: "services/masterimagesslider/creative/Logo-Design.jpg", link: "/services/logo-design" },
-  { title: "Social Media Content", bg: "services/masterimagesslider/creative/Social-Media-Content.jpg", link: "/services/social-media-content" },
-  { title: "Videography & Photography", bg: "services/masterimagesslider/creative/Videography-&-Photography.jpg", link: "/services/videography-and-photography" },
+  { title: "3D animation & rendering", bg: "services/masterimagesslider/creative/3D-Animation-&-Rendering.png", link: "services/3D-animation-and-rendering" },
+  { title: "Branding", bg: "services/masterimagesslider/creative/Branding.png", link: "/services/branding" },
+  { title: "Content Creation", bg: "services/masterimagesslider/creative/Content-Creation.png", link: "/services/content-creation" },
+  { title: "Logo Design", bg: "services/masterimagesslider/creative/Logo-Design.png", link: "/services/logo-design" },
+  { title: "Social Media Content", bg: "services/masterimagesslider/creative/Social-Media-Content.png", link: "/services/social-media-content" },
+  { title: "Videography & Photography", bg: "services/masterimagesslider/creative/Videography-&-Photography.png", link: "/services/videography-and-photography" },
 ];
 
 export const Digital = [
@@ -12,18 +12,16 @@ export const Digital = [
   { title: "google location services ads", bg: "services/masterimagesslider/digital/Google-Location-Services-Ads.jpg", link: "/services/google-location-services-ads" },
   { title: "PPC Campaigns", bg: "services/masterimagesslider/digital/PPC-Campaigns.jpg", link: "/services/ppc-campaigns" },
   { title: "Retargeting & Remarketing", bg: "services/masterimagesslider/digital/Retargeting-&-Remarketing.jpg", link: "/services/retargeting-and-remarketing" },
-  { title: "review & reputation management", bg: "services/masterimagesslider/digital/Review-and-Reputation-Management.jpg", link: "/services/review-and-reputation-management" },
+  { title: "review & reputation management", bg: "services/masterimagesslider/digital/Review-and-Reputation-Management.png", link: "/services/review-and-reputation-management" },
   { title: "SEO", bg: "services/masterimagesslider/digital/SEO.jpg", link: "/services/seo" },
-  { title: "Social Media Marketing", bg: "services/masterimagesslider/digital/Social-Media-Marketing.jpg", link: "/services/social-media-marketing" },
-  { title: "Website Development", bg: "services/masterimagesslider/digital/Website-Development.jpg", link: "/services/website-development" },
+  { title: "Social Media Marketing", bg: "services/masterimagesslider/digital/Social-Media-Marketing.png", link: "/services/social-media-marketing" },
+  { title: "Website Development", bg: "services/masterimagesslider/digital/Website-Development.png", link: "/services/website-development" },
 ];
 
 export const Industry = [
-  { title: "Solution for Lawyers", bg: "services/masterimagesslider/industry/Solution-for-Lawyers.jpg", link: "/services/solution-for-lawyers" },
+  { title: "Solution for Lawyers", bg: "services/masterimagesslider/industry/Solution-for-Lawyers.png", link: "/services/solution-for-lawyers" },
   { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.jpg", link: "/services/solution-for-doctors" },
   { title: "Solution for Restaurants", bg: "services/masterimagesslider/industry/Solution-for-Restaurant.jpg", link: "/services/solution-for-restaurants" },
   { title: "Solution for Tech Startups", bg: "services/masterimagesslider/industry/Solution-for-Tech-Startups.jpg", link: "/services/solution-for-techstartups" },
-   { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.jpg", link: "/services/solution-for-doctors" },
-  
-
+  { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.jpg", link: "/services/solution-for-doctors" },
 ];
