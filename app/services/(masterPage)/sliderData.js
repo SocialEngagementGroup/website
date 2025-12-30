63cab75f2f2409f861a907e181ae92a99301da14
Sunit sen <69672778+sunitsen@users.noreply.github.com>
2025-12-31 01:21:09 +0600

    fix issue

--- app/services/(masterPage)/sliderData.js
+++ app/services/(masterPage)/sliderData.js
@@ -8,20 +8,20 @@ export const CreativeData = [
 ];
 
 export const Digital = [
-  { title: "google business profile management", bg: "services/masterimagesslider/digital/Google-Business-Profile-Management.jpg", link: "/services/google-business-profile-management" },
-  { title: "google location services ads", bg: "services/masterimagesslider/digital/Google-Location-Services-Ads.jpg", link: "/services/google-location-services-ads" },
-  { title: "PPC Campaigns", bg: "services/masterimagesslider/digital/PPC-Campaigns.jpg", link: "/services/ppc-campaigns" },
-  { title: "Retargeting & Remarketing", bg: "services/masterimagesslider/digital/Retargeting-&-Remarketing.jpg", link: "/services/retargeting-and-remarketing" },
+  { title: "google business profile management", bg: "services/masterimagesslider/digital/Google-Business-Profile-Management.png", link: "/services/google-business-profile-management" },
+  { title: "google location services ads", bg: "services/masterimagesslider/digital/Google-Location-Services-Ads.png", link: "/services/google-location-services-ads" },
+  { title: "PPC Campaigns", bg: "services/masterimagesslider/digital/PPC-Campaigns.png", link: "/services/ppc-campaigns" },
+  { title: "Retargeting & Remarketing", bg: "services/masterimagesslider/digital/Retargeting-&-Remarketing.png", link: "/services/retargeting-and-remarketing" },
   { title: "review & reputation management", bg: "services/masterimagesslider/digital/Review-and-Reputation-Management.png", link: "/services/review-and-reputation-management" },
-  { title: "SEO", bg: "services/masterimagesslider/digital/SEO.jpg", link: "/services/seo" },
+  { title: "SEO", bg: "services/masterimagesslider/digital/SEO.png", link: "/services/seo" },
   { title: "Social Media Marketing", bg: "services/masterimagesslider/digital/Social-Media-Marketing.png", link: "/services/social-media-marketing" },
   { title: "Website Development", bg: "services/masterimagesslider/digital/Website-Development.png", link: "/services/website-development" },
 ];
 
 export const Industry = [
   { title: "Solution for Lawyers", bg: "services/masterimagesslider/industry/Solution-for-Lawyers.png", link: "/services/solution-for-lawyers" },
-  { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.jpg", link: "/services/solution-for-doctors" },
-  { title: "Solution for Restaurants", bg: "services/masterimagesslider/industry/Solution-for-Restaurant.jpg", link: "/services/solution-for-restaurants" },
-  { title: "Solution for Tech Startups", bg: "services/masterimagesslider/industry/Solution-for-Tech-Startups.jpg", link: "/services/solution-for-techstartups" },
-  { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.jpg", link: "/services/solution-for-doctors" },
+  { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.png", link: "/services/solution-for-doctors" },
+  { title: "Solution for Restaurants", bg: "services/masterimagesslider/industry/Solution-for-Restaurant.png", link: "/services/solution-for-restaurants" },
+  { title: "Solution for Tech Startups", bg: "services/masterimagesslider/industry/Solution-for-Tech-Startups.png", link: "/services/solution-for-techstartups" },
+  { title: "Solution for Doctors", bg: "services/masterimagesslider/industry/Solution-for-Doctors.png", link: "/services/solution-for-doctors" },
 ];
